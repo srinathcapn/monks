bb58612bf5f143a0a61e8c3e579338b16e8ef5db
CJ <srinathaprabhu@curejoy.com>
2017-12-18 23:29:37 +0530

    testing pixel code

--- index.php
+++ index.php
@@ -43,6 +43,25 @@ $display_past_events = display_all_past_events();
                         <li><a href="#testimonials">Testimonials</a></li>
                         <li><a href="contact.php">Contact Us</a></li>
                         <li class="hidden-xs"><a class="blue-btn" href="https://www.eventshigh.com/bangalore/monks+on+wheels">Book now</a></li>
+                        <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '867111826804660'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=867111826804660&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
                         <!--<li class="call-li hidden-xs" style="position: relative;">
                             <a class="call-btn" href="tel:080123456"><span class="sprite call"></span></a>
                             <div class="pos-abs" id="call-div">
